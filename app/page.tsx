b18ba0e7defc77acc0afe2ba83ec134009747d10
Ondřej Losenský <127244546+OndrejLosensky@users.noreply.github.com>
2025-01-13 20:53:12 +0100

    added page title

--- app/page.tsx
+++ app/page.tsx
@@ -40,6 +40,7 @@ const sidebarSections = [
 export default function Home() {
   return (
     <div className="max-w-4xl mx-auto px-6">      
+      <h1 className="py-4 text-3xl font-semibold"> Components Library v1 </h1>
       <div className="space-y-12">
         {sidebarSections.map((section, index) => (
           <div key={index} className="space-y-4">
